31830ff1fe04a9f31d61e8414e78ca571869a4c5
Gerald Bullard Jr III <gb@elykinnovation.com>
2025-08-07 00:27:17 -0400

    SAVE WORK

--- includes/languages/english/modules/navbar_modules/nb_brand.php
+++ includes/languages/english/modules/navbar_modules/nb_brand.php
@@ -13,5 +13,5 @@
   define('MODULE_NAVBAR_BRAND_TITLE', 'Brand');
   define('MODULE_NAVBAR_BRAND_DESCRIPTION', 'Show Brand in Navbar.  <div class="secWarning">This can be a simple link or something more complicated such as an image.<br><br>For more details about using an image, see <a target="_blank" href="http://getbootstrap.com/components/#navbar-brand-image"><u>#navbar-brand-image</u></a></div>');
   
-  define('MODULE_NAVBAR_BRAND_PUBLIC_TEXT', '<a class="navbar-brand" href="' . tep_href_link('index.php') . '">' . STORE_NAME . '</a>');
+  define('MODULE_NAVBAR_BRAND_PUBLIC_TEXT', '<a class="navbar-brand" style="padding:10px;" href="' . tep_href_link('index.php') . '"><img style="height:30px;width:auto;" src="images/general-logo.png" alt="Generic Store Logo" title="' . STORE_NAME . '" class="d-inline-block align-top" /></a>');
   
