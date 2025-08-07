f37665839bc4e6fb46c213183185e74400494138
Gerald Bullard Jr III <gb@elykinnovation.com>
2025-08-07 00:48:21 -0400

    Admin Footer

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -11,8 +11,13 @@
 */
 ?>
 
-<footer class="p-3 mb-0 bg-light text-dark fixed-bottom border-top">
+<footer class="p-2 mb-0 bg-light text-dark fixed-bottom border-top">
 	<div class="container">
-		<p class="text-muted text-center">osCommerce Online Merchant Copyright &copy; 2000-<?php echo date('Y'); ?> <a href="http://www.oscommerce.com" target="_blank">osCommerce</a> (<a href="http://www.oscommerce.com/Us&amp;Legal" target="_blank">Copyright and Trademark Policy</a>)</p>
+		<p class="mb-0 text-muted text-center">
+            osCommerce Online Merchant Copyright &copy; 2000-<?php echo date('Y'); ?> 
+            <a href="http://www.oscommerce.com" target="_blank">osCommerce</a> 
+            (<a href="http://www.oscommerce.com/Us&amp;Legal" target="_blank">Copyright and Trademark Policy</a>) 
+            Modified for <strong>PHP8+</strong> by: <a href="https://www.geraldbullardjr.com/" target="_blank">Forgotten Studios</a>
+        </p>
 	</div>
 </footer>
